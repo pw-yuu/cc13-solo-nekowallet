ab21f3604cc32df512f666ad9f3e842a09bba3d0
Philippe huynh <pw.yuupi@gmail.com>
2020-08-12 15:22:27 +0900

    Added cors

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const app = express();
 const db = require("./knex");
 const path = require("path")
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
 
 const PORT = process.env.PORT || 9000;
+
+app.use(cors())
+
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
